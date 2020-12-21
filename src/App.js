3868fe7e37472dca963da009e178121aeaa2a508
vuonga1103 <vuonga1103@yahoo.com>
2020-12-21 00:01:07 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (e) => {
+    setState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const {name, email, message} = state;
+    console.log(name, email, message)
+
+    const form = await axios.post('/api/form', {
+      name,
+      email,
+      message
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="name">Name</Label>
+        <Input type="text" name="name" onChange={handleChange}/>
+      </FormGroup>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input type="email" name="email" onChange={handleChange}/>
+      </FormGroup>
+      <FormGroup>
+        <Label for="message">Message</Label>
+        <Input type="text" name="message" onChange={handleChange}/>
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
   );
+
+  
+
 }
 
 export default App;
